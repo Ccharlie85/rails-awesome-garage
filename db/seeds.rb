917c58a7f46469d56d2a835fc24a1b6e8694f995
Carlos Belda Bustin <carlosbeldabustin@gmail.com>
2024-10-03 14:44:58 +0100

    updating seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,15 @@ Owner.destroy_all
 
 puts "Creating owners..." # Because it's the parent!
 
-Owner.create!(nickname: "TommyV")
-Owner.create!(nicknae:"Charlie")
+owner1 = Owner.create!(nickname: "TommyV")
+owner2 = Owner.create!(nicknae:"Charlie")
 
 puts "Creating cars..."
 
 Car.create!(brand: "Mercedes", model: "A-Class Hatchback", year: "2024", fuel: "petrol", owner: owner1)
 Car.create!(brand: "Peugeot", overview: "308", year: "2017", fuel: "petrol", owner: owner2)
+
+# car1 = Car.new(brand: "BMW", etc.....)
+# car1.save
 
 puts "Seed created! #{Owner.all count} owners created & #{Car.all.count}"
