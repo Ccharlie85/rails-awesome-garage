ecf2360ad5738ed6b6d8c43f8ca78e7631831975
Carlos Belda Bustin <carlosbeldabustin@gmail.com>
2024-10-03 14:25:28 +0100

    adding validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Car.create(brand: "Mercedes", model: "A-Class Hatchback", Year: "2024", owner_id: 1)
+Car.create(brand: "Peugeot", overview: "308", Year: "2017", owner_id: 2)
