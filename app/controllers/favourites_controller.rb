4b5c7efbb72260da157816c97cbb835c6b665a45
Carlos Belda Bustin <carlosbeldabustin@gmail.com>
2024-10-03 18:08:58 +0100

    updating index.html.erb

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,12 +1,24 @@
 class FavouritesController < ApplicationController
+  def index
+    @favourites = Favourite.new
+  end
+
+
+
   def create
     @favourite = Favourite.new
     @car = Car.find(params[:car_id])
-    @favourite.save
+    @favourite.save = @car
     if @favourite.save
       redirect_to car_path(@car)
     else
       render "car/show", status: :unprocessable_entity
     end
+  end
+
+  def destroy
+  @favourite = Fvourite.find(params[:id])
+  @favourite.destroy
+  redirect_to favourites_path, status: :see_other
   end
 end
