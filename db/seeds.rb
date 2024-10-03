e16cb48a058b05878894771f6ffd21a60627fba1
Carlos Belda Bustin <carlosbeldabustin@gmail.com>
2024-10-03 14:40:23 +0100

    updating seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
-Car.create(brand: "Mercedes", model: "A-Class Hatchback", Year: "2024", owner_id: 1)
-Car.create(brand: "Peugeot", overview: "308", Year: "2017", owner_id: 2)
+puts "Cleaning DB..."
+
+Review.destroy_all
+Favorite.destroy_all
+Car.destroy_all
+Owner.destroy_all
+
+puts "Creating owners..." # Because it's the parent!
+
+Owner.create!(nickname: "TommyV")
+Owner.create!(nicknae:"Charlie")
+
+puts "Creating cars..."
+
+Car.create!(brand: "Mercedes", model: "A-Class Hatchback", year: "2024", fuel: "petrol", owner: owner1)
+Car.create!(brand: "Peugeot", overview: "308", year: "2017", fuel: "petrol", owner: owner2)
+
+puts "Seed created! #{Owner.all count} owners created & #{Car.all.count}"
